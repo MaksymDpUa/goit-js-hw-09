3fbe53a6197b6ec9ceef465ded2365a27896c8bb
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-04-18 15:35:31 +0200

    finished task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+const formEl = document.querySelector('.form');
+const startBtn = document.querySelector('button');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.elements.delay.value);
+  const step = Number(event.currentTarget.elements.step.value);
+  const amount = Number(event.currentTarget.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+     createPromise([i], delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+     delay += step;
+  } 
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+       
+      } else {
+        reject({ position, delay });
+       
+      }
+    }, delay);
+  });
 }
